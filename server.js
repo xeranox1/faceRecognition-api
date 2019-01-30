3826c04aaa82153552c1e5afdbb13fe7c531317f
xeranox1 <albert_rusli@hotmail.com>
2019-01-30 11:52:27 +0700

    Adding Project

---
+++ server.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const bodyParser= require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'John',
+            email: 'john@gmail.com',
+            password: 'cookies',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Sally',
+            email: 'sally@gmail.com',
+            password: 'bananas',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+app.get('/', (req, res) => {
+    res.send(database.users);
+}) 
+
+app.post('/signin', (req, res) => {
+    if(req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
+        res.json(database.users[0]);
+    } else {
+        res.status(400).json('error logging in');
+    }
+})
+
+app.post('/register', (req, res) => {
+    const {email, name, password} = req.body;
+    bcrypt.hash(password, null, null, function(err, hash) {
+       console.log(hash);
+    });
+    database.users.push({
+        id: '125',
+        name: name,
+        email: email,
+        entries: 0,
+        joined: new Date()
+    })
+    res.json(database.users[database.users.length-1]);
+})
+
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if(user.id === id) {
+            found = true;
+            return res.json(user);
+        } 
+    });
+    if(!found) {
+        res.status(404).json('No such user');
+    }
+    
+})
+
+app.put('/image', (req, res) => {
+    const { id } = req.body;
+    let found = false;
+    database.users.forEach(user => {
+        if(user.id === id) {
+            found = true;
+            user.entries++;
+            return res.json(user.entries);
+        } 
+    });
+    if(!found) {
+        res.status(404).json('No such user');
+    }
+})
+
+app.listen(3000, ()=> {
+    console.log('app is running on port 3000');
+});
